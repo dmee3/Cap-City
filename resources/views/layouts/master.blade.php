6d98a5178a5ec536b132f2c209c271aeea3b9736
Dan Meehan <dan.meehan17@gmail.com>
2016-08-22 18:35:40 -0400

    Updated Zac and some images

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,23 @@
 <html>
 <head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<meta name="description" content="Cap City">
+	<meta name="author" content="Cap City">
+
+	<!-- Facebook Open Graph meta tags -->
+	<meta property="og:url" content="http://www.capcitypercussion.com">
+	<meta property="og:title" content="Cap City">
+	<meta property="og:description" content="WGI Percussion Independent World ensemble from Columbus, Ohio">
+	<meta property="og:image" content="http://www.capcitypercussion.com/img/fb_share_logo.png">
+
 	<title>Cap City</title>
 
 	<link rel="stylesheet" href="http://fonts.googleapis.com/icon?family=Material+Icons">
 	<link type="text/css" rel="stylesheet" href="css/style.css"/>
 	@yield('styles')
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<link rel="icon" type="image/png" href="img/icon.png">
 
 </head>
@@ -51,6 +63,7 @@
 	<div class="section cap-black center">
 		<a class="white-text" href="https://www.facebook.com/capitalcitypercussion/"><i class="fa fa-facebook fa-3x social-media" aria-hidden="true"></i></a>
 		<a class="white-text" href="https://twitter.com/CapCityPerc"><i class="fa fa-twitter fa-3x social-media" aria-hidden="true"></i></a>
+		<a class="white-text" href="https://www.youtube.com/channel/UCWjWCag6pALBDY5vmXMkTaw"><i class="fa fa-youtube-play fa-3x social-media" aria-hidden="true"></i></a>
 		<a class="white-text" href="https://www.instagram.com/capcitypercussion/"><i class="fa fa-instagram fa-3x social-media" aria-hidden="true"></i></a>
 		<a class="white-text" href="https://www.snapchat.com/add/snapcityperc"><i class="fa fa-snapchat fa-3x social-media" aria-hidden="true"></i></a>
 	</div>
@@ -88,6 +101,11 @@
 		Materialize.toast($toastContent, 5000, 'red');
 	</script>
 	@endif
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+		ga('create', 'UA-66433403-2', 'auto');
+		ga('send', 'pageview');
+	</script>
 </body>
 </html>
