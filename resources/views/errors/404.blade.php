45822febecac4482c53335f73ab91d4902a1878d
Dan Meehan <dan.meehan17@gmail.com>
2016-08-26 16:13:48 -0400

    Updated error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,18 @@
+<html class="cap-blue">
+<head>
+	<title>Cap City - Whoops</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body class="cap-blue">
+	<div class="container center white-text">
+		<div class="row">
+			<h2>Go Fish</h2>
+			<h5>That page doesn't exist.</h5>
+		</div>
+		<div class="row">
+			<img src="img/full_logo_transparent.png" style="width:100%;">
+		</div>
+	</div>
+</body>
+</html>
--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -6,10 +6,14 @@
 </head>
 <body class="cap-black">
 	<div class="container center white-text">
-		<h2>Whoops...</h2>
-		<h5>Something went wrong on our end.  Sorry about that!</h5>
-		<p class="thin">If you need to get ahold of us, please use the form <a href="http://www.capcitypercussion.com/contact" class="cap-red-text">here</a>.</p>
-		<img src="img/fb_share_logo.png" style="bottom: 0px; width: 100%;">
+		<div class="row">
+			<h2>Whoops...</h2>
+			<h5>Something went wrong on our end.  Sorry about that!</h5>
+			<p class="thin">If you need to get ahold of us, please use the form <a href="http://www.capcitypercussion.com/contact" class="cap-red-text">here</a>.</p>
+		</div>
+		<div class="row">
+			<img src="img/full_logo_transparent.png" style="width: 100%;">
+		</div>
 	</div>
 </body>
 </html>
