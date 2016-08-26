e0fdc20d48495b0cec3b958ec8a923877a02d6f6
Dan Meehan <dan.meehan17@gmail.com>
2016-08-26 15:52:07 -0400

    Custom error page

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,15 @@
+<html style="background-color: #323337;">
+<head>
+	<title>Cap City - Whoops</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body class="cap-black">
+	<div class="container center white-text">
+		<h2>Whoops...</h2>
+		<h5>Something went wrong on our end.  Sorry about that!</h5>
+		<p class="thin">If you need to get ahold of us, please use the form <a href="http://www.capcitypercussion.com/contact" class="cap-red-text">here</a>.</p>
+		<img src="img/fb_share_logo.png" style="bottom: 0px; width: 100%;">
+	</div>
+</body>
+</html>
