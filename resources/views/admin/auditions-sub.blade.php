a030111167e12602ad360246851e729959c7422a
dmee3 <dan.meehan17@gmail.com>
2016-09-12 15:26:22 -0400

    Admin registrations page update 2

--- resources/views/admin/auditions-sub.blade.php
+++ resources/views/admin/auditions-sub.blade.php
@@ -3,7 +3,15 @@
 	<div class="collapsible-body">
 		<ul class="collection">
 			@foreach ($data as $i)
-				<li class="collection-item">{{ $i->first_name . " " . $i->last_name }}</li>
+				<li class="collection-item">
+					{{ $i->first_name . " " . $i->last_name }}
+					<span class="secondary-content">
+						@if ($i->registered == 'true')
+							<i class="material-icons green-text">check_circle</i>
+						@else
+							<i class="material-icons orange-text">help_outline</i>
+						@endif
+				</li>
 			@endforeach
 		</ul>
 	</div>
