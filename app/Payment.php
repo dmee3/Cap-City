c8b2ab6230762fab31a174361889d38fd6d0edd6
Dan Meehan <dan.meehan17@gmail.com>
2016-09-17 22:21:34 -0400

    Added soft deletes to payments

--- app/Payment.php
+++ app/Payment.php
@@ -3,9 +3,36 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Payment extends Model
 {
+
+/**
+ * NOTE:
+ * type is stripe/check/cash
+ * info is check number
+ * category is incoming dues payment (0) or outgoing staff payment (1)
+ */
+
+	use softDeletes;
+
+	/**
+	 * The attributes that should be mutated to dates.
+	 *
+	 * @var array
+	 */
+	protected $dates = ['deleted_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'amount', 'date_paid', 'type', 'info', 'category'
+    ];
+
     /* Model relationships */
 	public function user() { return $this->belongsTo('App\User'); }
 }
