5266d993f65b749193e3d491cb0e79522f44fbfd
dmee3 <dan.meehan17@gmail.com>
2016-10-06 04:30:39 -0400

    Updated link in new user email

--- resources/views/emails/new-user.blade.php
+++ resources/views/emails/new-user.blade.php
@@ -5,7 +5,7 @@
 <p>Email: {{ $email }}</p>
 <p>Password: {{ $password }}</p>
 <br>
-<a href="https://www.capcitypercussion.com/login" style="border: none; border-radius: 2px; display: inline-block; color: #ffffff; background-color: #26a65b; height: 36px; line-height: 36px; outline: 0; padding: 0 2rem; text-transform: uppercase; vertical-align: middle; letter-spacing: 0.5px; text-decoration: none;">Go To Account</a>
+<a href="https://www.capcitypercussion.com/settings" style="border: none; border-radius: 2px; display: inline-block; color: #ffffff; background-color: #26a65b; height: 36px; line-height: 36px; outline: 0; padding: 0 2rem; text-transform: uppercase; vertical-align: middle; letter-spacing: 0.5px; text-decoration: none;">Go To Account</a>
 <br>
 <p>Thanks,</p>
 <p>Cap City Admin Team</p>
